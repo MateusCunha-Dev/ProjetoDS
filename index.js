aa30bde78a13c8906ef03584c85f47feba9a9c86
MateusCunha-Dev <mateuscunhadev@gmail.com>
2025-05-01 14:14:15 -0300

    evoluindo a autentificacao

--- index.js
+++ index.js
@@ -1,35 +1,37 @@
 $(document).ready(function() {
-    $("#formulario-login").validate({
+    $("#formulario").validate({
         rules: {
-            usuario: { required: true },
+            login: { required: true },  // ID "login" (como no PDF)
             senha: { required: true }
         },
         messages: {
-            usuario: "Campo obrigatório",
-            senha: "Campo obrigatório"
-        },
-        submitHandler: function(form) {
-            autenticar();
-            return false; // Evita o submit tradicional
+            login: "Campo obrigatório",
+            senha: "Campo obrigatórioo"
         }
     });
 });
 
 async function autenticar() {
-    const login = $("#usuario").val();
-    const senha = $("#senha").val();
-
-    try {
-        const resposta = await fetch(`https://api-odinline.odiloncorrea.com/usuario/${login}/${senha}/autenticar`);
-        const usuario = await resposta.json();
-
-        if (usuario.id > 0) {
-            localStorage.setItem('usuarioAutenticado', JSON.stringify(usuario));
-            window.location.href = "menu.html";
-        } else {
-            alert("Usuário ou senha inválidos.");
+    if ($("#formulario").valid()) {  // Validação mantida (como no PDF)
+        let login = $("#login").val();
+        let senha = $("#senha").val();
+        try {
+            let resposta = await fetch(`https://api-odinline.odiloncorrea.com/usuario/${login}/${senha}/autenticar`);
+            let usuario = await resposta.json();
+            if (usuario.id > 0) {
+                localStorage.setItem('usuarioAutenticado', JSON.stringify(usuario));
+                window.location.href = "menu.html";
+            } else {
+                alert("Usuário ou senha inválidos.");
+            }
+        } catch (error) {
+            alert("Erro ao tentar autenticar.");
         }
-    } catch (error) {
-        alert("Erro na conexão com a API. Tente novamente.");
     }
 }
+
+
+function sair() {
+    localStorage.removeItem("usuarioAutenticado");
+    window.location.href = "index.html";
+  }
